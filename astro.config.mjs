e7bb54790f9bd9fc9b55088d877d6e14184a4747
Noe Svanda <svanda.noe@gmail.com>
2026-06-15 20:45:28 +0200

    adjust astro config to match domain name

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://novosens.github.io",
-  base: "/novosense",
+  site: "https://novosense.cz",
   output: "static"
 });
